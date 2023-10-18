4d8bfef2dd33f8c723be7b0234ea27be5ea4244c
nik <nik@heartex.net>
2023-10-18 17:47:11 +0100

    Add basic tutorial

--- adala/agents/base.py
+++ adala/agents/base.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field, SkipValidation
 from abc import ABC, abstractmethod
-from typing import Any, Optional, List
+from typing import Any, Optional, List, Dict
 from adala.datasets.base import Dataset, BlankDataset
 from adala.runtimes.base import Runtime, LLMRuntime, LLMRuntimeModelType
 from adala.memories.base import ShortTermMemory, LongTermMemory
@@ -22,9 +22,30 @@ class Agent(BaseModel, ABC):
     """
     dataset: Optional[Dataset] = Field(default_factory=lambda: BlankDataset())
     memory: Optional[LongTermMemory] = Field(default=None)
-    runtime: Optional[Runtime] = Field(
-        default_factory=lambda: LLMRuntime(llm_runtime_type=LLMRuntimeModelType.OpenAI)
+    runtimes: Optional[Dict[str, Runtime]] = Field(
+        default_factory=lambda: {
+            'openai': LLMRuntime(
+                llm_runtime_type=LLMRuntimeModelType.OpenAI,
+                llm_params={
+                    'model': 'gpt-3.5-turbo-instruct',
+                }
+            ),
+            'openai-gpt4': LLMRuntime(
+                llm_runtime_type=LLMRuntimeModelType.OpenAI,
+                llm_params={
+                    'model': 'gpt-4',
+                }
+            ),
+            # 'llama2': LLMRuntime(
+            #     llm_runtime_type=LLMRuntimeModelType.Transformers,
+            #     llm_params={
+            #         'model': 'meta-llama/Llama-2-7b',
+            #         'device': 'cuda:0',
+            #     }
+            # )
+        }
     )
+    default_runtime: str = 'openai'
 
     @abstractmethod
     def greet(self) -> str:
--- adala/agents/single_shot.py
+++ adala/agents/single_shot.py
@@ -25,16 +25,20 @@ For example, try:
 agent.skill = adala.LabelingSkill(labels=['positive', 'negative'])'''
         print(greeting)
 
-    def run(self):
+    def run(self, runtime: str = None):
         if not self.skill:
             raise ValueError("Skill is not set")
-
-        experience = self.skill.apply(dataset=self.dataset, runtime=self.runtime)
+        runtime = runtime or self.default_runtime
+        experience = self.skill.apply(dataset=self.dataset, runtime=self.runtimes[runtime])
         return AgentStep(experience=experience, is_last=True)
 
-    def learn(self):
-        experience = self.skill.learn(self.dataset, runtime=self.runtime, memory=self.memory)
+    def learn(self, runtime: str = None, update_instructions: bool = False):
+        runtime = runtime or self.default_runtime
+        experience = self.skill.learn(self.dataset, runtime=self.runtimes[runtime], memory=self.memory)
         if self.memory:
             self.memory.remember(experience)
+
+        if update_instructions:
+            self.skill.instructions = experience.updated_instructions
 
         return AgentStep(experience=experience, is_last=True)
