69faf91c5c67f33a10bd1b397f29495bad1bd256
nik <nik@heartex.net>
2023-11-02 12:34:54 +0000

    Remove unused import

--- adala/skills/__init__.py
+++ adala/skills/__init__.py
@@ -1,6 +1,5 @@
 from .skillset import SkillSet, LinearSkillSet
 from .labeling.classification import LLMSkill, ClassificationSkill, ClassificationSkillWithCoT
-from .labeling.sequence_labeling import SequenceLabelingSkill
 from .generation.base import TextGenerationSkill
 from .generation.qa import QuestionAnsweringSkill
 from .generation.summarization import SummarizationSkill
