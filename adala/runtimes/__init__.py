4410c8adfd92f4de6105358a3f3aa222aacac75f
nik <nik@heartex.net>
2023-11-14 13:48:37 +0000

    Import langchain runtime only if installed

--- adala/runtimes/__init__.py
+++ adala/runtimes/__init__.py
@@ -1,4 +1,9 @@
 from .base import Runtime
 from ._openai import OpenAIChatRuntime, OpenAIVisionRuntime
 from ._guidance import GuidanceRuntime, GuidanceModelType
-from ._langchain import LangChainRuntime
+
+try:
+    # check if langchain is installed
+    from ._langchain import LangChainRuntime
+except ImportError:
+    pass
