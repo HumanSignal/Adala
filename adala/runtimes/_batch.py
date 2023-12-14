616168edd2a266f3c5b3f3e6465be4f0f1c9430c
nik <nik@heartex.net>
2023-12-14 14:29:26 +0000

    Add vanilla fallback

--- adala/runtimes/_batch.py
+++ adala/runtimes/_batch.py
@@ -1,30 +1,65 @@
+from tqdm import tqdm
 from adala.utils.internal_data import InternalDataFrame, InternalDataFrameConcat
 from adala.runtimes.base import Runtime
 from typing import Optional, Dict, Any, List
-from vllm import LLM, SamplingParams
+try:
+    from vllm import LLM, SamplingParams
+    from transformers import AutoTokenizer, AutoModelForCausalLM
+    _VLLM_AVAILABLE = True
+except ImportError:
+    _VLLM_AVAILABLE = False
 from pydantic import Field
 from adala.utils.parse import parse_template, partial_str_format
 from adala.utils.matching import match_options
-from transformers import AutoTokenizer
+from ._openai import chat_completion_call
 
 
 class BatchRuntime(Runtime):
 
-    vllm_model: str = Field(alias="model", default_factory=lambda: "mistralai/Mistral-7B-Instruct-v0.2")
+    runtime_model: str = Field(alias="model", default_factory=lambda: "mistralai/Mistral-7B-Instruct-v0.2")
 
     _llm = None
     _tokenizer = None
     _max_tokens = None
 
     def init_runtime(self) -> "Runtime":
-        self._llm = LLM(model=self.vllm_model)
-        self._tokenizer = AutoTokenizer.from_pretrained(self.vllm_model)
+        if _VLLM_AVAILABLE:
+            self._init_vllm()
+        else:
+            self._init_vanilla()
         return self
+
+    def _init_vllm(self):
+        self._llm = LLM(model=self.runtime_model)
+        self._tokenizer = AutoTokenizer.from_pretrained(self.runtime_model)
+
+    def _init_vanilla(self):
+        pass
 
     def _convert(self, string):
         return self._tokenizer.apply_chat_template([{'role': 'user', 'content': string}], tokenize=False)
 
     def execute(self, prompts, options):
+        if not _VLLM_AVAILABLE:
+            # fallback to vanilla OpenAI API calls
+            completions = []
+            for prompt in tqdm(prompts):
+                messages = [{'role': 'user', 'content': prompt}]
+                if self.verbose:
+                    print(f'Prompt: {prompt}')
+                completion = chat_completion_call('gpt-4-1106-preview', messages)
+                completion_text = completion.choices[0].message.content
+                if options:
+                    completion_option = match_options(completion_text, options)
+                    if self.verbose:
+                        print(f'Completion text: {completion_text}, matched option: {completion_option}')
+                    completions.append(completion_option)
+                else:
+                    if self.verbose:
+                        print(f'Completion text: {completion_text}')
+                    completions.append(completion_text)
+            return completions
+
         if not self._max_tokens:
             self._max_tokens = max(map(lambda o: len(self._tokenizer.tokenize(o)), options))
         params = SamplingParams(max_tokens=self._max_tokens)
