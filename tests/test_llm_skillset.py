49b34ab6760baa810eef935afabcb5f87e52c7f0
nik <nik@heartex.net>
2024-01-11 15:57:22 +0000

    Make relative imports

--- tests/test_llm_skillset.py
+++ tests/test_llm_skillset.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from .utils import patching, PatchedCalls
+from utils import patching, PatchedCalls
 
 
 @patching(
--- tests/test_openai_runtime.py
+++ tests/test_openai_runtime.py
@@ -1,4 +1,4 @@
-from .utils import patching, PatchedCalls, mdict
+from utils import patching, PatchedCalls, mdict
 
 
 @patching(
