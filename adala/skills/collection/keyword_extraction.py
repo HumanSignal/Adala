10b88e7989758f21ac41afdbc8e1ca22c30fc8b2
makseq-ubnt <makseq@gmail.com>
2024-02-21 04:19:54 +0000

    Fixes

--- adala/skills/collection/keyword_extraction.py
+++ adala/skills/collection/keyword_extraction.py
@@ -14,40 +14,38 @@ class KeywordExtractionSkill(TransformSkill):
 
     class LabelItem(BaseModel):
         name: str
-        instruction: str  # Assuming instruction is a string. Adjust the type as needed.
+        instruction: str  # instruction on how to use this label
 
     name: str = "keyword_extraction"
     instructions: str = '''
-Given the following text, identify and extract the keywords. Assign a label for each keyword. \
-It is crucial to extract the keywords exactly as they appear in the text, maintaining their 
-original form and capitalization. \
-If a word appears multiple times, each occurrence should be labeled in accordance with its 
-specific context or form in the text. \
+## LABELS
 
 ## Instructions:
 
+1. Read the Provided Text Thoroughly: Carefully examine the entire content of the text you are given.
+2. Identify Keywords: Detect and note down the keywords within the text. Keywords are defined as words or phrases that carry significant meaning or relevance in relation to the context of the text and are directly associated with the LABELS specified above.
+3. Preserve Original Order and Spelling: List the identified keywords in the exact sequence they occur within the text, maintaining their original spelling and capitalization.
+4. Assign Labels to Keywords: For each identified keyword, you must allocate an appropriate label from the LABELS listed above. Each label should closely match the context or category the keyword pertains to.
+5. Explanation: For each identified keyword, you must write a short explanation why you used this label.
+6. Handle Multiple Occurrences: If a keyword is found multiple times in the text, and its context or form varies, you should list and label each instance separately, treating them as distinct entries.
+7. Format Your Response: Present the keywords and their respective labels in the format 'keyword-or-phrase // label // explanation'. Your response should solely consist of a list of these entries, each on a new line, arranged in the order they appear within the text.
+8. Exclusive Output Requirement: Your output should exclusively contain the list of identified keywords and their corresponding labels, formatted as instructed. Exclude any additional text or commentary from your response.
+'''
+
+    """
 1. Read the text thoroughly.
 2. Identify the keywords in the text. Keywords are words that hold significant meaning or importance 
-in the context of the text and related to LABELS only. 
-3. Extract these words exactly as they appear, preserving their original  spelling and capitalization.
-4. For each keyword, assign a label from the provided LABELS.
-5. If a keyword appears multiple times in different contexts or forms, label each occurrence separately.
+in the context of the text and related to the provided LABELS above. 
+3. List these keywords exactly at the same order as they appear in the Input, preserving their original spelling and capitalization.
+4. For each keyword, assign a label from the provided LABELS above.
+5. If a keyword appears multiple times in different contexts or forms, list and label each occurrence separately.
 6. List the keywords from text (the same as they appear in the text) and their 
    corresponding labels in the order they appear in the text in the format 'keyword-or-phrase // label'.
 7. Do not output any text except the newline separated list of keywords and corresponding labels.
-
-## Example 
-Note: LABELS will be different, it's just an example
-Input: """Deep learning improves image processing. Learning models adapt to new images."""
-Output:
-Deep learning // Field
-image processing // Task
-Learning // Action
-images // Object
-'''
+"""
     input_template: str = 'Input:\n"""\n{text}\n"""'
     output_template: str = "Output:\n{keywords}"
-    labels: Optional[Dict[str, List[Dict['name':str, 'instruction':str]]]]
+    labels: Optional[Dict[str, List[LabelItem]]]
 
     @model_validator(mode="after")
     def validate_labels(self):
@@ -65,12 +63,17 @@ images // Object
                 )
 
         # add label list to instructions
-        # TODO: doesn't work for multiple outputs
-        self.instructions += "\n## LABELS\nUse the following labels only:\n"
-        labels_list = "\n".join(self.labels[output_fields[0]])
-        self.instructions += f"{labels_list}\n\n"
+        labels = self.labels[output_fields[0]]
+        labels_list = "\n".join([
+            f'{i}. "{label.name}"' +
+            (" - " + label.instruction) if label.instruction else '' +
+            '.'
+            for i, label in enumerate(labels)
+        ])
+        self.instructions = self.instructions.replace('## LABELS', '## LABELS\n' + labels_list)
 
-    def _postprocess_output(self, row, output_field: str) -> str:
+    def _postprocess_output(self, row, output_field: str) -> list:
+        labels = [label.name for label in self.labels[output_field]]
         output_string = row[output_field]
         text = row["text"]
 
@@ -78,17 +81,20 @@ images // Object
 
         start_index = 0
         output = []
-        print('==>', output_string)
+        print('==>', text, '\n', output_string)
 
         for line in output_string.split('\n'):
+            if not line:
+                continue
+
             split = line.split('//')
-            if len(split) != 2:
+            if len(split) != 3:
                 print_error(f"Keyword and label couldn't be parsed: {line}")
                 continue
 
-            keyword, label = split[0].strip(), split[1].strip()
-            if label not in self.labels[output_field]:
-                print_error(f"Label '{label}' not in provided labels: {self.labels[output_field]}")
+            keyword, label, explain = split[0].strip(), split[1].strip(), split[2].strip()
+            if label not in labels:
+                print_error(f"Label '{label}' not in provided labels: {labels}")
                 continue
 
             index = text.find(keyword, start_index)
@@ -96,7 +102,9 @@ images // Object
                 output.append({
                     'start': index,
                     'end': index + len(keyword),
-                    'labels': [label]
+                    'labels': [label],
+                    'text': keyword,
+                    'explain': explain
                 })
                 start_index = index + len(keyword)
             else:
@@ -113,7 +121,6 @@ images // Object
         runtime.verbose = True
         batch = super(KeywordExtractionSkill, self).apply(input, runtime)
         input_batch = InternalDataFrameConcat((input, batch), axis=1)
-        print(batch)
 
         # apply postprocessing with keyword extraction from output
         for output_field in self.get_output_fields():
