8c62173c29003c2626733166a5dd0496658c4704
makseq-ubnt <makseq@gmail.com>
2024-02-21 04:20:25 +0000

    Fix

--- adala/skills/collection/keyword_extraction.py
+++ adala/skills/collection/keyword_extraction.py
@@ -31,18 +31,6 @@ class KeywordExtractionSkill(TransformSkill):
 7. Format Your Response: Present the keywords and their respective labels in the format 'keyword-or-phrase // label // explanation'. Your response should solely consist of a list of these entries, each on a new line, arranged in the order they appear within the text.
 8. Exclusive Output Requirement: Your output should exclusively contain the list of identified keywords and their corresponding labels, formatted as instructed. Exclude any additional text or commentary from your response.
 '''
-
-    """
-1. Read the text thoroughly.
-2. Identify the keywords in the text. Keywords are words that hold significant meaning or importance 
-in the context of the text and related to the provided LABELS above. 
-3. List these keywords exactly at the same order as they appear in the Input, preserving their original spelling and capitalization.
-4. For each keyword, assign a label from the provided LABELS above.
-5. If a keyword appears multiple times in different contexts or forms, list and label each occurrence separately.
-6. List the keywords from text (the same as they appear in the text) and their 
-   corresponding labels in the order they appear in the text in the format 'keyword-or-phrase // label'.
-7. Do not output any text except the newline separated list of keywords and corresponding labels.
-"""
     input_template: str = 'Input:\n"""\n{text}\n"""'
     output_template: str = "Output:\n{keywords}"
     labels: Optional[Dict[str, List[LabelItem]]]
