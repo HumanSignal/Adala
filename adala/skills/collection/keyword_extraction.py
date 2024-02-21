23e7b4cf1548c07cf0b8782fd686ffa627d578ad
makseq-ubnt <makseq@gmail.com>
2024-02-21 04:51:18 +0000

    Fix

--- adala/skills/collection/keyword_extraction.py
+++ adala/skills/collection/keyword_extraction.py
@@ -18,18 +18,21 @@ class KeywordExtractionSkill(TransformSkill):
 
     name: str = "keyword_extraction"
     instructions: str = '''
-## LABELS
-
+## LABELS    
+    
 ## Instructions:
 
 1. Read the Provided Text Thoroughly: Carefully examine the entire content of the text you are given.
-2. Identify Keywords: Detect and note down the keywords within the text. Keywords are defined as words or phrases that carry significant meaning or relevance in relation to the context of the text and are directly associated with the LABELS specified above.
+2. Identify Keywords: Detect and note down the keywords within the text. Keywords are defined as words or phrases that carry significant meaning or relevance in relation to the context of the text and are directly associated with the LABELS specified.
 3. Preserve Original Order and Spelling: List the identified keywords in the exact sequence they occur within the text, maintaining their original spelling and capitalization.
-4. Assign Labels to Keywords: For each identified keyword, you must allocate an appropriate label from the LABELS listed above. Each label should closely match the context or category the keyword pertains to.
+4. Assign Labels to Keywords: For each identified keyword, you must allocate an appropriate label from the LABELS listed. Each label should closely match the context or category the keyword pertains to.
 5. Explanation: For each identified keyword, you must write a short explanation why you used this label.
 6. Handle Multiple Occurrences: If a keyword is found multiple times in the text, and its context or form varies, you should list and label each instance separately, treating them as distinct entries.
 7. Format Your Response: Present the keywords and their respective labels in the format 'keyword-or-phrase // label // explanation'. Your response should solely consist of a list of these entries, each on a new line, arranged in the order they appear within the text.
 8. Exclusive Output Requirement: Your output should exclusively contain the list of identified keywords and their corresponding labels, formatted as instructed. Exclude any additional text or commentary from your response.
+9. Label Format: LABELS can include per-label instructions on how to use this label in format: <number_of_label>. "<label_name>" - <instructions>. 
+10. If no label is assigned to a keyword, you don't need to list this keyword. 
+11. If two or more keywords go together, you can stack them to keyword phrase and label together.  
 '''
     input_template: str = 'Input:\n"""\n{text}\n"""'
     output_template: str = "Output:\n{keywords}"
@@ -54,8 +57,7 @@ class KeywordExtractionSkill(TransformSkill):
         labels = self.labels[output_fields[0]]
         labels_list = "\n".join([
             f'{i}. "{label.name}"' +
-            (" - " + label.instruction) if label.instruction else '' +
-            '.'
+            (" - " + label.instruction + '.') if label.instruction else ''
             for i, label in enumerate(labels)
         ])
         self.instructions = self.instructions.replace('## LABELS', '## LABELS\n' + labels_list)
