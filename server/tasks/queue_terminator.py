39a136f87cb98d1099c753d5371a160dd442224d
Sergey Zhuk <sergey.zhuk@humansignal.com>
2024-05-15 10:01:03 +0100

    ci: PLT-353: add graceful shutdown script for celery (#115)

---
+++ server/tasks/queue_terminator.py
@@ -0,0 +1,90 @@
+"""
+Script to stop consumer connections from Celery workers.
+
+This script is used to stop the consumer connections for specified Celery queues. If no queues are
+specified, the script will stop consumer connections for all active queues. The script ensures that
+all running jobs are completed before exiting.
+
+Usage:
+    python script_name.py [queue1 queue2 ...]
+
+Arguments:
+    queues (optional): Names of queues to stop consumer connections for. If not provided, all active
+                       queues will be considered.
+
+Functions:
+    stop_consumer_for_queues(queues):
+        Stops the consumer connections for the specified Celery queues. If no queues are specified,
+        it inspects the Celery workers to find all active queues and stops the consumer connections
+        for them. Waits until all running jobs are finished before exiting.
+
+    main():
+        Main function to parse command-line arguments and invoke the stop_consumer_for_queues function.
+        Accepts a list of queue names as arguments. If no queue names are provided, it stops consumer
+        connections for all active queues.
+
+Entry Point:
+    The script is intended to be run as a standalone program. The entry point calls the main function
+    to parse arguments and stop consumer connections for queues.
+"""
+
+import time
+import argparse
+import logging
+import sys
+from celery.app.control import Control
+from process_file import app as celery
+
+logger = logging.getLogger(__name__)
+
+
+def stop_consumer_for_queues(queues):
+    control = Control(celery)
+
+    if not queues:
+        inspect = control.inspect()
+        queues_info = inspect.active_queues()
+        if queues_info:
+            queues = {
+                queue["name"]
+                for worker_queues in queues_info.values()
+                for queue in worker_queues
+            }
+        else:
+            logger.info("No active queues found.")
+            return
+
+    for queue_name in queues:
+        logger.info(f"Cancel consumer for queue: {queue_name}")
+        control.cancel_consumer(queue_name)
+
+    inspect = control.inspect()
+    while True:
+        active = inspect.active()
+        running_jobs = [job for value in active.values() for job in value]
+        if len(running_jobs) > 0:
+            logger.info(
+                f"{len(running_jobs)} jobs running: {', '.join(job['name'] for job in running_jobs)}"
+            )
+            time.sleep(10)
+        else:
+            logger.info("No running jobs")
+            break
+
+    sys.exit(0)  # Exit the script after all jobs have finished
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Script to stop consumer connections from Celery worker"
+    )
+    parser.add_argument(
+        "queues", nargs="*", help="Names of queues to stop consumer connections for"
+    )
+    args = parser.parse_args()
+
+    stop_consumer_for_queues(args.queues)
+
+
+if __name__ == "__main__":
+    main()
