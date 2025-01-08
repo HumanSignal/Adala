d83ff6ff8cdc9540856ed7213244a3488d5df361
hakan458 <hakan@heartex.com>
2025-01-07 17:23:34 -0800

    feat: DIA-1715: VertexAI Gemini model support

--- adala/runtimes/_litellm.py
+++ adala/runtimes/_litellm.py
@@ -50,6 +50,8 @@ from .base import AsyncRuntime, Runtime
 
 logger = logging.getLogger(__name__)
 
+# TODO remove
+litellm.drop_params = True
 
 # basically only retrying on timeout, incomplete output, or rate limit
 # https://docs.litellm.ai/docs/exception_mapping#custom-mapping-list
@@ -153,6 +155,22 @@ def _get_usage_dict(usage: Usage, model: str) -> Dict:
     return data
 
 
+def resolve_litellm_model_and_provider(model_name: str, provider: str):
+    """
+    When using litellm.get_model_info() some models are accessed with their provider prefix
+    while others are not.
+
+    This helper function contains logic which resolves this for supported providers
+    """
+    if "/" in model_name:  # TODO handle models like vertex_ai/meta/llama ...
+        model_name = model_name.split("/")[1]
+    provider = provider.lower()
+    if provider == "vertexai":
+        provider = "vertex_ai"
+
+    return model_name, provider
+
+
 class InstructorClientMixin:
     def _from_litellm(self, **kwargs):
         return instructor.from_litellm(litellm.completion, **kwargs)
@@ -160,7 +178,7 @@ class InstructorClientMixin:
     @cached_property
     def client(self):
         kwargs = {}
-        if self.is_custom_openai_endpoint:
+        if self.is_custom_openai_endpoint or self.model.startswith("vertex"):
             kwargs["mode"] = instructor.Mode.JSON
         return self._from_litellm(**kwargs)
 
@@ -402,7 +420,7 @@ class AsyncLiteLLMChatRuntime(InstructorAsyncClientMixin, AsyncRuntime):
                 model=self.model,
                 max_tokens=self.max_tokens,
                 temperature=self.temperature,
-                seed=self.seed,
+                # seed=self.seed,
                 # extra inference params passed to this runtime
                 **self.model_extra,
             )
@@ -553,9 +571,12 @@ class AsyncLiteLLMChatRuntime(InstructorAsyncClientMixin, AsyncRuntime):
         return user_tokens + system_tokens
 
     @staticmethod
-    def _get_completion_tokens(model: str, output_fields: Optional[List[str]]) -> int:
+    def _get_completion_tokens(
+        model: str, output_fields: Optional[List[str]], provider: str
+    ) -> int:
+        model, provider = resolve_litellm_model_and_provider(model, provider)
         max_tokens = litellm.get_model_info(
-            model=model, custom_llm_provider="openai"
+            model=model, custom_llm_provider=provider
         ).get("max_tokens", None)
         if not max_tokens:
             raise ValueError
@@ -565,10 +586,14 @@ class AsyncLiteLLMChatRuntime(InstructorAsyncClientMixin, AsyncRuntime):
 
     @classmethod
     def _estimate_cost(
-        cls, user_prompt: str, model: str, output_fields: Optional[List[str]]
+        cls,
+        user_prompt: str,
+        model: str,
+        output_fields: Optional[List[str]],
+        provider: str,
     ):
         prompt_tokens = cls._get_prompt_tokens(user_prompt, model, output_fields)
-        completion_tokens = cls._get_completion_tokens(model, output_fields)
+        completion_tokens = cls._get_completion_tokens(model, output_fields, provider)
         prompt_cost, completion_cost = litellm.cost_per_token(
             model=model,
             prompt_tokens=prompt_tokens,
@@ -579,7 +604,11 @@ class AsyncLiteLLMChatRuntime(InstructorAsyncClientMixin, AsyncRuntime):
         return prompt_cost, completion_cost, total_cost
 
     def get_cost_estimate(
-        self, prompt: str, substitutions: List[Dict], output_fields: Optional[List[str]]
+        self,
+        prompt: str,
+        substitutions: List[Dict],
+        output_fields: Optional[List[str]],
+        provider: str,
     ) -> CostEstimate:
         try:
             user_prompts = [
@@ -594,6 +623,7 @@ class AsyncLiteLLMChatRuntime(InstructorAsyncClientMixin, AsyncRuntime):
                     user_prompt=user_prompt,
                     model=self.model,
                     output_fields=output_fields,
+                    provider=provider,
                 )
                 cumulative_prompt_cost += prompt_cost
                 cumulative_completion_cost += completion_cost
@@ -729,8 +759,9 @@ class AsyncLiteLLMVisionRuntime(AsyncLiteLLMChatRuntime):
 
     def init_runtime(self) -> "Runtime":
         super().init_runtime()
-        if not litellm.supports_vision(self.model):
-            raise ValueError(f"Model {self.model} does not support vision")
+        # model_name = self.model
+        # if not litellm.supports_vision(model_name):
+        #     raise ValueError(f"Model {self.model} does not support vision")
         return self
 
     async def batch_to_batch(
@@ -816,7 +847,10 @@ class AsyncLiteLLMVisionRuntime(AsyncLiteLLMChatRuntime):
 
     # TODO: cost estimate
 
-def get_model_info(provider: str, model_name: str, auth_info: Optional[dict]=None) -> dict:
+
+def get_model_info(
+    provider: str, model_name: str, auth_info: Optional[dict] = None
+) -> dict:
     if auth_info is None:
         auth_info = {}
     try:
@@ -826,11 +860,11 @@ def get_model_info(provider: str, model_name: str, auth_info: Optional[dict]=Non
                 model=f"azure/{model_name}",
                 messages=[{"role": "user", "content": ""}],
                 max_tokens=1,
-                **auth_info
+                **auth_info,
             )
             model_name = dummy_completion.model
-        full_name = f"{provider}/{model_name}"
-        return litellm.get_model_info(full_name)
+        model_name, provider = resolve_litellm_model_and_provider(model_name, provider)
+        return litellm.get_model_info(model=model_name, custom_llm_provider=provider)
     except Exception as err:
         logger.error("Hit error when trying to get model metadata: %s", err)
         return {}
