8d6baaf88a43a66e9789a6880591f79d6ad0b4dd
Matt Bernstein <matt@humansignal.com>
2025-01-10 14:30:21 -0500

    validate gemini API keys

--- server/app.py
+++ server/app.py
@@ -248,6 +248,7 @@ async def validate_connection(request: ValidateConnectionRequest):
     multi_model_provider_test_models = {
         "openai": "gpt-4o-mini",
         "vertexai": "vertex_ai/gemini-1.5-flash",
+        "gemini": "gemini/gemini-1.5-flash",
     }
     provider = request.provider.lower()
     messages = [{"role": "user", "content": "Hey, how's it going?"}]
@@ -255,7 +256,7 @@ async def validate_connection(request: ValidateConnectionRequest):
     # For multi-model providers use a model that every account should have access to
     if provider in multi_model_provider_test_models.keys():
         model = multi_model_provider_test_models[provider]
-        if provider == "openai":
+        if provider in {"openai", "gemini"}:
             model_extra = {"api_key": request.api_key}
         elif provider == "vertexai":
             model_extra = {"vertex_credentials": request.vertex_credentials}
@@ -325,7 +326,7 @@ async def models_list(request: ModelsListRequest):
     # https://docs.litellm.ai/docs/set_keys#get_valid_models
     # https://github.com/BerriAI/litellm/blob/b9280528d368aced49cb4d287c57cd0b46168cb6/litellm/utils.py#L5705
     # Ultimately just uses litellm.models_by_provider - setting API key is not needed
-    lse_provider_to_litellm_provider = {"openai": "openai", "vertexai": "vertex_ai"}
+    lse_provider_to_litellm_provider = {"openai": "openai", "vertexai": "vertex_ai", "gemini": "gemini"}
     provider = request.provider.lower()
     valid_models = litellm.models_by_provider[
         lse_provider_to_litellm_provider[provider]
